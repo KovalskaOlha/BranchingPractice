e8abd8c1311b5ba2a17dbd7ec8b87d1d77b0b8f8
KovalskaOlha <unlimolg@gmail.com>
2023-03-29 13:43:41 -0400

    First practice

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, I'm practicing");
+        Something s=new Something();
+        System.out.println(s.num);
+    }
+}
---
+++ src/Something.java
@@ -0,0 +1,5 @@
+public class Something {
+    String hope="i hope everything will be good";
+    int num=110;
+    boolean isTrue=true;
+}
